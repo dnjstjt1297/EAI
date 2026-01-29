9c8336181dbab43011b1569747768eb3ac30f0a0
dnjstjt1297 <dnjstjt1297@gmail.com>
2026-01-30 01:20:52 +0900

    feat: rest api Exception -#6

---
+++ src/main/java/global/exception/RestApiException.java
@@ -0,0 +1,17 @@
+package main.java.global.exception;
+
+import main.java.global.exception.errorcode.ErrorCode;
+
+public class RestApiException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public RestApiException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
