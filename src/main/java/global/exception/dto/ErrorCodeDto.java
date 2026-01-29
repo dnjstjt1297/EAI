09349a83c0cf1e7788159839132b9675c6288a4f
dnjstjt1297 <dnjstjt1297@gmail.com>
2026-01-30 02:06:46 +0900

    fix: Valid null -#6

--- src/main/java/global/exception/dto/ErrorCodeDto.java
+++ src/main/java/global/exception/dto/ErrorCodeDto.java
@@ -1,9 +1,13 @@
 package main.java.global.exception.dto;
 
+import lombok.NonNull;
 import main.java.global.httpserver.enums.HttpStatus;
 
+@NonNull
 public record ErrorCodeDto(
+        @NonNull
         String message,
+        @NonNull
         HttpStatus status
 ) {
 
