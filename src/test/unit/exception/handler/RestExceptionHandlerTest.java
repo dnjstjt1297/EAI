f622610506d385ae9b828249bb6bbc46c4abcbe3
dnjstjt1297 <dnjstjt1297@gmail.com>
2026-01-31 06:30:30 +0900

    test: RestExceptionHandler unit test -#8

--- src/test/unit/exception/handler/RestExceptionHandlerTest.java
+++ src/test/unit/exception/handler/RestExceptionHandlerTest.java
@@ -1,36 +1,37 @@
 package test.unit.exception.handler;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import main.java.global.exception.RestApiException;
 import main.java.global.exception.dto.ErrorCodeDto;
 import main.java.global.exception.errorcode.enums.CommonErrorCode;
-import main.java.global.exception.errorcode.enums.TcpErrorCode;
+import main.java.global.exception.errorcode.enums.HttpServerErrorCode;
 import main.java.global.exception.handler.RestApiExceptionHandler;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class ExceptionHandlerUnitTest {
+public class RestExceptionHandlerTest {
 
     RestApiExceptionHandler restApiExceptionHandler = new RestApiExceptionHandler();
 
     @Test
-    @DisplayName("RestApiException 발생 시 설정된 에러 코드와 상태 코드를 반환해야 한다")
+    @DisplayName("RestApiException 발생 시 설정된 에러 코드와 상태 코드를 반환할 수 있다.")
     void handleRestApiException() throws Exception {
         // given
-        Exception e = new RestApiException(TcpErrorCode.INVALID_REQUEST);
+        Exception e = new RestApiException(HttpServerErrorCode.INVALID_REQUEST);
 
         // when
         ErrorCodeDto result = restApiExceptionHandler.handle(e);
 
         // then
-        assertEquals(TcpErrorCode.INVALID_REQUEST.getStatus(), result.status());
-        assertTrue(result.message().contains(TcpErrorCode.INVALID_REQUEST.getMessage()));
+        assertEquals(HttpServerErrorCode.INVALID_REQUEST.getStatus(), result.status());
+        assertTrue(result.message().contains(HttpServerErrorCode.INVALID_REQUEST.getMessage()));
     }
 
     @Test
-    @DisplayName("IllegalArgumentException 발생 시: INVALID_PARAMETER 코드를 반환해야 한다")
+    @DisplayName("IllegalArgumentException 발생 시 INVALID_PARAMETER 코드를 반환반환할 수 있다.")
     void handleIllegalArgumentException() throws Exception {
         // given
         Exception e = new IllegalArgumentException("잘못된 인자");
@@ -43,7 +44,7 @@ public class ExceptionHandlerUnitTest {
     }
 
     @Test
-    @DisplayName("3. 기타 일반 Exception 발생 시: INTERNAL_SERVER_ERROR를 반환해야 한다")
+    @DisplayName("기타 일반 Exception 발생 시 INTERNAL_SERVER_ERROR를 반환할 수 있다.")
     void handleGeneralException() throws Exception {
         // given
         Exception e = new RuntimeException("DB 서버 다운");
