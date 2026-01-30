71db3288164e917f88d1e5753404b70a04563dcc
dnjstjt1297 <dnjstjt1297@gmail.com>
2026-01-31 06:19:53 +0900

    feat: Add error code -#8

--- src/main/java/global/exception/errorcode/enums/CommonErrorCode.java
+++ src/main/java/global/exception/errorcode/enums/CommonErrorCode.java
@@ -6,9 +6,9 @@ import main.java.global.httpserver.enums.HttpStatus;
 public enum CommonErrorCode implements ErrorCode {
 
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "잘못된 파라미터 입니다."),
-    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "자원이 존재하지 않습니다."),
+    NOTFOUND_RESOURCE(HttpStatus.NOT_FOUND, "자원이 존재하지 않습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 에러입니다."),
-    UNKNOWN_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "에러 메시지 생성 실패");
+    UNKNOWN_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "에러 메시지 생성 실패했습니다.");
 
     private final HttpStatus status;
     private final String message;
