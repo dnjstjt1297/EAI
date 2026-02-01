f2051ff6708f7da07acf0a74eb6d705558b27dda
dnjstjt1297 <dnjstjt1297@gmail.com>
2026-02-02 06:42:41 +0900

    feat: Handle Properties -#10

---
+++ src/main/java/global/properties/AppProperties.java
@@ -0,0 +1,25 @@
+package main.java.global.properties;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class AppProperties {
+
+    private final Properties properties;
+
+    public void init(String path) {
+        try (InputStream inputStream = getClass().getResourceAsStream(path)) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+}
