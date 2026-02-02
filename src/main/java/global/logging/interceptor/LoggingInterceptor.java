4b273ebce8185dd4d91354202317f50420803163
dnjstjt1297 <dnjstjt1297@gmail.com>
2026-02-03 06:15:30 +0900

    feat & fix: Add logging interceptor & Fix transaction interceptor -#12

---
+++ src/main/java/global/logging/interceptor/LoggingInterceptor.java
@@ -0,0 +1,64 @@
+package main.java.global.logging.interceptor;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import lombok.AllArgsConstructor;
+import main.java.global.logging.LogContext;
+import main.java.global.logging.annotation.LogExecution;
+import main.java.global.proxy.ProxyWrapper;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@AllArgsConstructor
+public class LoggingInterceptor implements MethodInterceptor, ProxyWrapper {
+
+    private final Object target;
+    private final LogContext logContext;
+    private static final Logger log = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public Object intercept(Object object, Method method, Object[] args, MethodProxy proxy)
+            throws Throwable {
+        if (method.isAnnotationPresent(LogExecution.class)) {
+            LogExecution logExecution = method.getAnnotation(LogExecution.class);
+
+            String indent = logContext.getIndent();
+            String className = target.getClass().getSimpleName();
+
+            log.info("{}[START] {}.{}()", indent, className, method.getName());
+
+            logContext.increment();
+
+            long startTime = System.currentTimeMillis();
+            try {
+
+                Object result = method.invoke(target, args);
+                logContext.decrement();
+
+                log.info("{}[END] {}.{}() , Duration: {}ms", indent, className,
+                        method.getName(), System.currentTimeMillis() - startTime);
+                return result;
+            } catch (Exception e) {
+
+                Exception exception = e;
+                while (exception instanceof InvocationTargetException ite) {
+                    exception = (Exception) ite.getTargetException();
+                }
+
+                logContext.decrement();
+                log.error("{}[ERROR] {}.{}() , Exception: {} , Duration: {}ms",
+                        indent, className, method.getName(), exception.getMessage(),
+                        System.currentTimeMillis() - startTime);
+                throw exception;
+            }
+        }
+        return proxy.invoke(target, args);
+    }
+
+    @Override
+    public Object getTarget() {
+        return target;
+    }
+}
