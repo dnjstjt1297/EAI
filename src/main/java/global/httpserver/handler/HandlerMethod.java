1f59a906dd04546523b234c547c153cc4de99dcd
dnjstjt1297 <dnjstjt1297@gmail.com>
2026-02-03 14:59:41 +0900

    fix: Remove comment -#14

--- src/main/java/global/httpserver/handler/HandlerMethod.java
+++ src/main/java/global/httpserver/handler/HandlerMethod.java
@@ -9,8 +9,8 @@ import main.java.global.httpserver.dto.response.HttpResponse;
 @AllArgsConstructor
 public class HandlerMethod {
 
-    private final Object bean; // 호출할 컨트롤러
-    private final Method method;  // 실행할 메서드 정보 (리플렉션)
+    private final Object bean;
+    private final Method method;
 
     public HttpResponse invoke(Object... args) throws Exception {
         return (HttpResponse) method.invoke(bean, args);
